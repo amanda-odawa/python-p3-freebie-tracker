abdf4fb83c6b989ad662dcb6e1113d3ff5ea6ea4
AmandaOdawa <amandaodawa1@gmail.com>
2025-03-04 10:15:33 +0300

    Done

---
+++ lib/migrations/versions/0158fa6a3759_freebies_migration.py
@@ -0,0 +1,24 @@
+"""Freebies migration
+
+Revision ID: 0158fa6a3759
+Revises: 5f72c58bf48c
+Create Date: 2025-03-04 09:57:47.500824
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0158fa6a3759'
+down_revision = '5f72c58bf48c'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
